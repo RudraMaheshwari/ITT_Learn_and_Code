295539b2b28416b79169123bcb09633074e4a837
Rudransh Maheshwari <maheshwarirudransh79@gmail.com>
2025-12-07 18:44:23 +0530

    Fixed naming convention for adjacent console

--- naming_conventions_assignment/country_code_application/adjacent_country_console.py
+++ naming_conventions_assignment/country_code_application/adjacent_country_console.py
@@ -4,7 +4,7 @@ class AdjacentCountryConsole:
     def __init__(self):
         self.country_finder = AdjacentCountryFinder()
 
-    def run_program(self):
+    def find_adjacent_countries(self):
         print("=== Adjacent Country Finder ===")
         country_code = input("Enter Country Code (e.g., IN, US, NZ): ").strip()
         self.country_finder.display_adjacent_countries(country_code)
--- naming_conventions_assignment/country_code_application/main.py
+++ naming_conventions_assignment/country_code_application/main.py
@@ -2,4 +2,4 @@ from adjacent_country_console import AdjacentCountryConsole
 
 if __name__ == "__main__":
     app = AdjacentCountryConsole()
-    app.run_program()
+    app.find_adjacent_countries()

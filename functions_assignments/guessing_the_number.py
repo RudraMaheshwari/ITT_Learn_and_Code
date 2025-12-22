2700882b0c113391bec79b72647779278f79f7ab
Rudransh Maheshwari <maheshwarirudransh79@gmail.com>
2025-12-22 15:23:35 +0530

    Splitted the condition logic into different files.

--- functions_assignments/guessing_the_number.py
+++ functions_assignments/guessing_the_number.py
@@ -9,18 +9,30 @@ def get_valid_guess():
         guess = input("I wont count this one Please enter a number between 1 to 100:")
     return int(guess)
 
+def is_too_low(guess, target):
+    return guess < target
+
+def is_too_high(guess, target):
+    return guess > target
+
+def check_guess(guess, target, guess_count):
+    if is_too_low(guess, target):
+        print("Too low. Guess again")
+        return False
+    elif is_too_high(guess, target):
+        print("Too High. Guess again")
+        return False
+    else:
+        print("You guessed it in", guess_count, "guesses!")
+        return True
+
 def play_number_guessing_game():
     target_number = random.randint(1, 100)
     guess_count = 0
     while True:
         guess = get_valid_guess()
         guess_count += 1
-        if guess < target_number:
-            print("Too low. Guess again")
-        elif guess > target_number:
-            print("Too High. Guess again")
-        else:
-            print("You guessed it in", guess_count, "guesses!")
+        if check_guess(guess, target_number, guess_count):
             break
 
 play_number_guessing_game()
